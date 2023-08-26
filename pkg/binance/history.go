11972fa9922aa129070447d11ffd93914e5723b9
Hwang <syhwang.web@gmail.com>
2023-08-26 15:01:59 +0900

    fix

--- pkg/binance/history.go
+++ pkg/binance/history.go
@@ -4,8 +4,7 @@ import (
 	"bytes"
 	"encoding/csv"
 	"fmt"
-	"log"
-	"sort"
+	"math"
 	"strconv"
 	"strings"
 	"time"
@@ -41,19 +40,44 @@ func FetchAllHistory(cfg *config.Config, bucketName, fileName string) (string, e
 		}
 
 		histories := filterHistoriesForAccount(updatedHistories, acc.Label)
-		for _, history := range histories {
-			log.Printf("Date: %s, Account: %s, Balance: %f", history.Date, history.Account, history.Balance)
+		monthHistories := filterHistoriesForThisMonth(histories)
+
+		if len(monthHistories) < 2 {
+			return "", fmt.Errorf("insufficient data for account: %s", acc.Label)
+		}
+		initialBalance := monthHistories[0].Balance
+		totalDelta := (float64(balance) - initialBalance) / initialBalance * 100
+
+		totalResults.WriteString(fmt.Sprintf(":bank: %s [%d (%+.2f%%)]\n", acc.Label, int(balance), totalDelta))
+		totalResults.WriteString(strings.Repeat("-", 40) + "\n")
+
+		for i, history := range monthHistories {
+			delta := "(+0%)"
+			if i > 0 {
+				prevBalance := monthHistories[i-1].Balance
+				deltaValue := (history.Balance - prevBalance) / prevBalance * 100
+				roundedDelta := int(math.Round(deltaValue))
+				delta = fmt.Sprintf("(%+d%%)", roundedDelta)
+			}
+			totalResults.WriteString(fmt.Sprintf("%d.%d: %d %s\n", history.Date.Month(), history.Date.Day(), int(history.Balance), delta))
 		}
 
-		calculatedReturns, err := calculateReturns(histories, acc.Label)
-		if err != nil {
-			return "", err
-		}
-
-		totalResults.WriteString(calculatedReturns)
+		totalResults.WriteString(strings.Repeat("-", 40) + "\n")
+		totalResults.WriteString("\n\n")
 	}
-
 	return totalResults.String(), nil
+}
+
+func filterHistoriesForThisMonth(histories []BalanceHistory) []BalanceHistory {
+	var monthHistories []BalanceHistory
+	currentMonth := time.Now().Month()
+
+	for _, history := range histories {
+		if history.Date.Month() == currentMonth {
+			monthHistories = append(monthHistories, history)
+		}
+	}
+	return monthHistories
 }
 
 func DBClear(bucketName, fileName string) error {
@@ -176,68 +200,4 @@ func filterHistoriesForAccount(allHistories []BalanceHistory, accountLabel strin
 		}
 	}
 	return filteredHistories
-}
-
-func calculateReturns(histories []BalanceHistory, accLabel string) (string, error) {
-	if len(histories) == 0 {
-		return "", fmt.Errorf("no history found for account: %s", accLabel)
-	}
-
-	sort.Slice(histories, func(i, j int) bool {
-		return histories[i].Date.Before(histories[j].Date)
-	})
-
-	initialBalance := histories[0].Balance
-	currentBalance := histories[len(histories)-1].Balance
-
-	var overallReturn float64
-	if initialBalance != 0 {
-		overallReturn = (currentBalance - initialBalance) / initialBalance * 100.0
-	} else {
-		overallReturn = 0
-	}
-
-	yesterday := time.Now().AddDate(0, 0, -1)
-	dailyReturn := calculateReturnFromDaysAgo(histories, currentBalance, yesterday, accLabel)
-
-	oneWeekAgo := time.Now().AddDate(0, 0, -7)
-	weeklyReturn := calculateReturnFromDaysAgo(histories, currentBalance, oneWeekAgo, accLabel)
-
-	oneMonthAgo := time.Now().AddDate(0, -1, 0)
-	monthlyReturn := calculateReturnFromDaysAgo(histories, currentBalance, oneMonthAgo, accLabel)
-
-	dailyReturnStr := formatReturn(dailyReturn)
-	weeklyReturnStr := formatReturn(weeklyReturn)
-	monthlyReturnStr := formatReturn(monthlyReturn)
-
-	var resultBuilder strings.Builder
-	resultBuilder.WriteString(":bank: " + accLabel + "\n")
-	resultBuilder.WriteString(strings.Repeat("-", 40) + "\n")
-	resultBuilder.WriteString(fmt.Sprintf("Total: %s (%.0f → %.0f)\n", formatReturn(overallReturn), initialBalance, currentBalance))
-	resultBuilder.WriteString(fmt.Sprintf("1D: %s\n", dailyReturnStr))
-	resultBuilder.WriteString(fmt.Sprintf("1W: %s\n", weeklyReturnStr))
-	resultBuilder.WriteString(fmt.Sprintf("1M: %s\n", monthlyReturnStr))
-	resultBuilder.WriteString(strings.Repeat("-", 40) + "\n")
-	resultBuilder.WriteString("\n\n")
-
-	return resultBuilder.String(), nil
-}
-
-func calculateReturnFromDaysAgo(histories []BalanceHistory, currentBalance float64, daysAgo time.Time, accountLabel string) float64 {
-	for _, history := range histories {
-		if history.Date.Year() == daysAgo.Year() && history.Date.Month() == daysAgo.Month() && history.Date.Day() == daysAgo.Day() {
-			if history.Balance == 0 {
-				return 0
-			}
-			return (currentBalance - history.Balance) * 100.0 / history.Balance
-		}
-	}
-	return 0
-}
-
-func formatReturn(val float64) string {
-	if val == 0 {
-		return "-"
-	}
-	return fmt.Sprintf("%.0f%%", val)
 }
