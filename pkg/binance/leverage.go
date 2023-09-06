ad01d7488c9f9b32e703947078227bf53a47f363
Hwang <syhwang.web@gmail.com>
2023-09-07 03:58:39 +0900

    update leverage, retry

--- pkg/binance/leverage.go
+++ pkg/binance/leverage.go
@@ -1,6 +1,7 @@
 package binance
 
 import (
+	"fmt"
 	"net/url"
 	"strconv"
 	"time"
@@ -8,15 +9,27 @@ import (
 
 func changeLeverage(apiKey, secretKey, symbol string, leverage int) error {
 	endpoint := "/fapi/v1/leverage"
-	params := url.Values{}
-	params.Add("symbol", symbol)
-	params.Add("leverage", strconv.Itoa(leverage))
-	params.Add("timestamp", strconv.FormatInt(time.Now().UnixNano()/1e6, 10))
-	sig := createHmac(params.Encode(), secretKey)
-	params.Add("signature", sig)
+	retryCount := 0
+	maxRetries := 5
 
-	_, err := sendRequest(apiKey, "POST", baseURL+endpoint, params)
-	return err
+	for retryCount < maxRetries {
+		params := url.Values{}
+		params.Add("symbol", symbol)
+		params.Add("leverage", strconv.Itoa(leverage))
+		params.Add("timestamp", strconv.FormatInt(time.Now().UnixNano()/1e6, 10))
+		sig := createHmac(params.Encode(), secretKey)
+		params.Add("signature", sig)
+
+		_, err := sendRequest(apiKey, "POST", baseURL+endpoint, params)
+		if err == nil {
+			return nil
+		}
+
+		leverage--
+		retryCount++
+	}
+
+	return fmt.Errorf("failed to change leverage after %d retries", maxRetries)
 }
 
 func changeMarginType(apiKey, secretKey, symbol, marginType string) error {
